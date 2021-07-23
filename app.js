1f4cbfad21545e81b8ab106af74ca3cbd73cc62a
yesseniarod <yessi.rod30@gmail.com>
2021-07-23 22:08:08 +0000

    create a function to start the game. #1

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
+
+const startGame = () => {
+  const playButton = document.querySelector('.intro button');
+  const introScreen = document.querySelector('.intro');
+  const match = document.querySelector('.match');
+
+}
